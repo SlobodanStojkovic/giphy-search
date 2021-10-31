8398c330b7c51675c66239c54caa5c207c203488
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-10-31 10:41:18 +0100

    Change to import useState from react in Page Navigation

--- src/components/PageNavigation/PageNavigation.jsx
+++ src/components/PageNavigation/PageNavigation.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import "./PageNavigation.css";
 
 export const PageNavigation = ({ clickedPage, setClickedPage }) => {
