745f16425f5e45f259398ffe15955fc051e7a28a
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-10-31 10:56:37 +0100

    Changing getGiphy fetch url from http to https

--- src/services/getGiphy.js
+++ src/services/getGiphy.js
@@ -1,7 +1,7 @@
 const apiKey = "WgOIx0GBcvOzAsdv9FxICFs7PXeK5IKv";
 
 export const getGiphy = (search) => {
-    return fetch("http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=50")
+    return fetch("https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=50")
         .then(data => data.json())
         .then(info => info)
 }
